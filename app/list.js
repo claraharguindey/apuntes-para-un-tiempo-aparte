659ac8fdbd3c37f8eace7dfefa48e2b81e9f499f
claraharguindey <clarisaharguindey@gmail.com>
2024-01-02 12:32:12 +0100

    Update styles add figure table

---
+++ app/list.js
@@ -0,0 +1,48 @@
+const table = document.getElementById("table");
+const content = [
+  {
+    figure: 1,
+    ref: "Instituto del Tiempo Suspendido",
+  },
+  {
+    figure: 2,
+    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
+  },
+  {
+    figure: 1,
+    ref: "Instituto del Tiempo Suspendido",
+  },
+  {
+    figure: 2,
+    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
+  },
+  {
+    figure: 1,
+    ref: "Instituto del Tiempo Suspendido",
+  },
+  {
+    figure: 2,
+    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
+  },
+  {
+    figure: 1,
+    ref: "Instituto del Tiempo Suspendido",
+  },
+  {
+    figure: 2,
+    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
+  },
+  {
+    figure: 1,
+    ref: "Instituto del Tiempo Suspendido",
+  },
+  {
+    figure: 2,
+    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
+  },
+];
+
+table.innerHTML =  content.map(
+  (item) =>
+    `<tr><td class="table-image-cell"><img src='/assets/media/hands/${item.figure}.png' alt='hands playing catscraddle' class="table-image"></td><td>${item.ref}</td></tr>`
+).join("");
