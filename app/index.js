d00a7a5c283259177c986bce7c3ef130febffd4c
claraharguindey <clarisaharguindey@gmail.com>
2024-01-03 14:02:29 +0100

    fix hyperlink

--- app/index.js
+++ app/index.js
@@ -2,12 +2,11 @@ const catscraddle = document.getElementById("catscraddle");
 const apuntes = document.getElementById("apuntes");
 const ephemeralText = document.getElementById("ephemeral");
 
-
 let handsCounter = 1;
 let textCounter = 1;
 
 const updateImage = () => {
-  catscraddle.src = `./../assets/media/hands/${handsCounter}.jpeg`;
+  catscraddle.src = `./assets/media/hands/${handsCounter}.jpeg`;
 
   if (handsCounter % 19 !== 0) {
     handsCounter++;
