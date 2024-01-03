0e87316f6353327e4bd028cd5fbbc730f406eec2
claraharguindey <clarisaharguindey@gmail.com>
2024-01-03 13:59:25 +0100

    Update images and add data

--- app/index.js
+++ app/index.js
@@ -2,40 +2,14 @@ const catscraddle = document.getElementById("catscraddle");
 const apuntes = document.getElementById("apuntes");
 const ephemeralText = document.getElementById("ephemeral");
 
-const content = [
-  {
-    figure: 1,
-    text: "El delirio de querer vivir muchos ritmos en una sola vida...",
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 1,
-    text: "Viene de tan lejos en el pasado e incumbe tanto al futuro que su fundación se encuentra, suspendida, dentro y fuera del tiempo...",
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 1,
-    text: "Hacia la reapropiación del tiempo expropiado, robado, perdido por todas las prácticas que reducen la cronodiversidad constitutiva de los seres...",
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 1,
-    text: "Un agujero negro dentro de internet porque atrae y altera el tiempo de quien la visita...",
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 1,
-    text: "Somos, cada cual, tiempos...",
-    ref: "Instituto del Tiempo Suspendido",
-  },
-];
+
 let handsCounter = 1;
 let textCounter = 1;
 
 const updateImage = () => {
-  catscraddle.src = `./../assets/media/hands/${handsCounter}.png`;
+  catscraddle.src = `./../assets/media/hands/${handsCounter}.jpeg`;
 
-  if (handsCounter % 2 !== 0) {
+  if (handsCounter % 19 !== 0) {
     handsCounter++;
   } else {
     handsCounter = 1;
@@ -47,7 +21,7 @@ const updateContent = () => {
     updateImage();
     ephemeralText.innerHTML = `<p class="ephemeralText">${content[textCounter].text}</p>`;
     apuntes.innerText += ` ${content[textCounter].text}`;
-    if (textCounter % 2 !== 0) {
+    if (textCounter % 19 !== 0) {
       textCounter++;
     } else {
       textCounter = 1;
--- app/list.js
+++ app/list.js
@@ -1,48 +1,21 @@
 const table = document.getElementById("table");
-const content = [
-  {
-    figure: 1,
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 2,
-    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
-  },
-  {
-    figure: 1,
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 2,
-    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
-  },
-  {
-    figure: 1,
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 2,
-    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
-  },
-  {
-    figure: 1,
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 2,
-    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
-  },
-  {
-    figure: 1,
-    ref: "Instituto del Tiempo Suspendido",
-  },
-  {
-    figure: 2,
-    ref: "Elisabeth Freeman, Time Binds: Queer Temporalities, Queer Histories",
-  },
-];
 
-table.innerHTML =  content.map(
-  (item) =>
-    `<tr><td class="table-image-cell"><img src='./../assets/media/hands/${item.figure}.png' alt='hands playing catscraddle' class="table-image"></td><td>${item.ref}</td></tr>`
-).join("");
+const ids = content.map(({ author }) => author);
+const filtered = content
+  .filter(({ author }, index) => !ids.includes(author, index + 1))
+  .sort(function (a, b) {
+    if (a.author < b.author) {
+      return -1;
+    }
+    if (a.author > b.author) {
+      return 1;
+    }
+    return 0;
+  });
+
+table.innerHTML = filtered
+  .map(
+    (item) =>
+      `<tr><td class="table-image-cell"><img src='./../assets/media/hands/${item.figure}.jpeg' alt='hands playing catscraddle' class="table-image"></td><td class="table-author">${item.author}</td> <td>${item.title}</td></tr>`
+  )
+  .join("");
