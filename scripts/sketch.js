13afd72d0df0e9f03235872362bd67dfad054de2
claraharguindey <clarisaharguindey@gmail.com>
2024-01-08 11:33:45 +0100

    Refactor

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,12 +1,14 @@
-let dots = [];
-let currentIndex = 0;
 const intro = document.getElementById("intro");
 const header = document.getElementById("header");
 const apuntesWrapper = document.getElementById("apuntesWrapper");
+const arrow = document.getElementById("arrow");
+
+let dots = [];
+let currentIndex = 0;
 let lastPos = { x: 0, y: 0 };
 let currentPos = { x: 0, y: 0 };
 let dotSize = 8;
-
+let started = false;
 
 class Dot {
   constructor(x, y) {
@@ -33,25 +35,25 @@ class Dot {
   }
 }
 
-let monoSynth;
-let started = false;
-
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
+
   if (started) {
-    canvas.mousePressed(playOscillator);
+    canvas.mousePressed(setPosition);
   }
-  currentPos.x = mouseX;
-  currentPos.y = mouseY;
+
+  setPosition();
   dots.push(new Dot(308, 332));
+
   const introPlayed = sessionStorage.getItem("introPlayed");
+
   if (introPlayed === "true") {
     lastPos.x = 308;
     lastPos.y = 332;
     currentIndex++;
-    started = true;
     apuntesWrapper.style.animation = "none";
     header.style.animation = "none";
+    started = true;
   } else {
     playIntro();
   }
@@ -66,6 +68,7 @@ function draw() {
       dots[i].connect(dots[i - 1].x, dots[i - 1].y);
     }
   }
+
   if (currentIndex == 0) {
     fill(255, 255, 255);
     stroke(255, 255, 255);
@@ -89,86 +92,93 @@ function mouseMoved() {
   }
 }
 
-function playOscillator() {
+function setPosition() {
   currentPos.x = mouseX;
   currentPos.y = mouseY;
 }
 
 function mouseReleased() {
   if (started) {
-    currentPos.x = mouseX;
-    currentPos.y = mouseY;
+    setPosition();
     dots.push(new Dot(mouseX, mouseY));
     currentIndex++;
     lastPos.x = mouseX;
     lastPos.y = mouseY;
+    if (arrow.style.display !== "block") {
+      arrow.style.display = "block";
+    }
   }
 }
 
-const restart = () => {
-  dots = [];
-};
+const restart = () => (dots = []);
+
+const introSteps = [
+  {
+    text: "Aquí recogemos momentos",
+    src: "./assets/media/intro/1.jpeg",
+    position: { x: 426, y: 415 },
+  },
+  {
+    text: "que tuvieron lugar",
+    src: "./assets/media/intro/2.jpeg",
+    position: { x: 622, y: 371 },
+    delay: 3000,
+  },
+  {
+    text: "en un tiempo aparte.",
+    src: "./assets/media/intro/3.jpeg",
+    position: { x: 715, y: 428 },
+    delay: 6000,
+  },
+  {
+    text: "De forma colectiva e infinita",
+    src: "./assets/media/intro/4.jpeg",
+    position: { x: 830, y: 376 },
+    delay: 9000,
+  },
+  {
+    text: "dibujamos una línea del tiempo",
+    src: "./assets/media/intro/5.jpeg",
+    position: { x: 851, y: 400 },
+    delay: 12000,
+  },
+  {
+    text: "que se ha vuelto constelación",
+    src: "./assets/media/intro/6.jpeg",
+    position: { x: 715, y: 428 },
+    delay: 15000,
+  },
+  {
+    text: "para pensar y rehacer",
+    src: "./assets/media/intro/7.jpeg",
+    position: { x: 735, y: 468 },
+    delay: 18000,
+  },
+  {
+    text: "Apuntes para un tiempo aparte.",
+    src: "./assets/media/intro/1.jpeg",
+    position: { x: 735, y: 468 },
+    customPosition: true,
+    delay: 21000,
+  },
+];
 
 const playIntro = () => {
-  intro.innerHTML = `<p class="introText">Aquí recogemos momentos</p>`;
-  catscraddle.src = `./assets/media/intro/1.jpeg`;
-  currentIndex++;
+  introSteps.forEach((step, index) => {
+    setTimeout(() => {
+      dots.push(new Dot(step.position.x, step.position.y));
+      intro.innerHTML = `<p class="introText">${step.text}</p>`;
+      catscraddle.src = step.src;
+      currentIndex++;
 
-  setTimeout(() => {
-    dots.push(new Dot(426, 415));
-    intro.innerHTML = `<p class="introText">que tuvieron lugar</p>`;
-    catscraddle.src = `./assets/media/intro/2.jpeg`;
-    currentIndex++;
-  }, 3000);
-
-  setTimeout(() => {
-    dots.push(new Dot(622, 371));
-    intro.innerHTML = `<p class="introText">en un tiempo aparte.</p>`;
-    catscraddle.src = `./assets/media/intro/3.jpeg`;
-
-    currentIndex++;
-  }, 6000);
-
-  setTimeout(() => {
-    dots.push(new Dot(715, 428));
-    intro.innerHTML = `<p class="introText">De forma colectiva e infinita</p>`;
-    catscraddle.src = `./assets/media/intro/4.jpeg`;
-
-    currentIndex++;
-  }, 9000);
-
-  setTimeout(() => {
-    dots.push(new Dot(830, 376));
-    intro.innerHTML = `<p class="introText">dibujamos una línea del tiempo</p>`;
-    catscraddle.src = `./assets/media/intro/5.jpeg`;
-
-    currentIndex++;
-  }, 12000);
-
-  setTimeout(() => {
-    dots.push(new Dot(851, 400));
-    intro.innerHTML = `<p class="introText">que se ha vuelto constelación</p>`;
-    catscraddle.src = `./assets/media/intro/6.jpeg`;
-    currentIndex++;
-  }, 15000);
-
-  setTimeout(() => {
-    dots.push(new Dot(715, 428));
-    intro.innerHTML = `<p class="introText">para pensar y rehacer</p>`;
-    catscraddle.src = `./assets/media/intro/7.jpeg`;
-    currentIndex++;
-  }, 18000);
-
-  setTimeout(() => {
-    dots.push(new Dot(mouseX, mouseY));
-    intro.innerHTML = `<p class="introText">Apuntes para un tiempo aparte.</p>`;
-    catscraddle.src = `./assets/media/intro/1.jpeg`;
-    currentPos.x = mouseX;
-    currentPos.y = mouseY;
-    lastPos.x = mouseX;
-    lastPos.y = mouseY;
-    currentIndex++;
-    started = true;
-    sessionStorage.setItem('introPlayed', true);
-  }, 21000);
+      if (step.customPosition) {
+        dots.push(new Dot(mouseX, mouseY));
+        setPosition();
+        lastPos.x = mouseX;
+        lastPos.y = mouseY;
+        started = true;
+        sessionStorage.setItem("introPlayed", true);
+      }
+    }, step.delay || index * 3000);
+  });
 };
